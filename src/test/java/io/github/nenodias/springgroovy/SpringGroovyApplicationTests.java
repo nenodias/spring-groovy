ac6e59002d39d3582b4d205e500856990e9ac56f
Horácio Dias <horacio.baptista@supportweb.com>
2018-05-11 16:06:03 -0300

    Refactoring pom

--- src/test/java/io/github/nenodias/springgroovy/SpringGroovyApplicationTests.java
+++ src/test/java/io/github/nenodias/springgroovy/SpringGroovyApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.nenodias.springjruby;
+package io.github.nenodias.springgroovy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringJrubyApplicationTests {
+public class SpringGroovyApplicationTests {
 
 	@Test
 	public void contextLoads() {
